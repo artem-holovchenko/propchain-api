236fae457defa189f4e4b954d905f7a0a4c6d813
Юрий Топчий <uriitopcii@Urijs-MacBook-Pro.local>
2021-12-03 15:42:42 +0200

    bulk upload

--- src/common/files.service.ts
+++ src/common/files.service.ts
@@ -51,13 +51,13 @@ export class FilesService {
         let gFiles = [];
         let rejFiles = [];
 
-        for await (let f of files) {
+        for (let f of files) {
             result.push(cloudinary.uploader.upload(f.path, { public_id: f.originalname }));
         }
 
         const results = await Promise.allSettled(result);
 
-        for await (let res of results) {
+        for (let res of results) {
             if (res.status === 'fulfilled') {
                 gFiles.push(this.filesDBRepository.create({ name: res.value.public_id, url: res.value.url }));
             } else {
@@ -68,11 +68,55 @@ export class FilesService {
             }
         }
 
+
+
+
         await this.delFiles(files);
         return gFiles;
 
          } catch (e) {
            throw new InternalServerErrorException();
          }
+    }
+
+
+    //TODO: divide and conquer
+    async uploadFile(file: Express.Multer.File) {
+        let uploadResult;
+        let dbResult;
+
+        try {
+            uploadResult = await cloudinary.uploader.upload(file.path);
+            await this.delFile(file);
+        } catch (e) {
+            throw new Error('failed to upload file')
+        }
+
+        try {
+            dbResult = await this.filesDBRepository.create({ name: uploadResult.value.public_id, url: uploadResult.value.url });
+        } catch (e) {
+            await cloudinary.uploader.destroy(uploadResult.value.public_id);
+            throw new Error('failed to save file data')
+        }
+
+        return dbResult;
+    }
+
+    async uploadBulk(files: Array<Express.Multer.File>) {
+        const uploads = [];
+        // Cloudinary free 10 concurrent requests
+        for(let i = 0; i<files.length; i++) {
+            uploads.push(this.uploadFile(files[i]));
+        }
+
+        const uploaded = await Promise.allSettled(uploads);
+
+        return uploaded.map((res)=>{
+            return {
+                status: res.status === 'fulfilled' ? 'uploaded' : 'failed',
+                name: res.status === 'fulfilled' ? res.value.public_id : 'none'
+            }
+        })
+
     }
 }
