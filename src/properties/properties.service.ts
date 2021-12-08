bcc3394d3fb0f7aeb79011ce455e3b471b0494b1
Artem <art2937@gmail.com>
2021-12-08 14:44:24 +0200

    fix(properties): fix methods in properties service

--- src/properties/properties.service.ts
+++ src/properties/properties.service.ts
@@ -10,15 +10,15 @@ export class PropertiesService {
     ) { }
 
     async createProperty(property: IProperties, files: Array<Express.Multer.File>): Promise<IProperties> {
-        return await this.propertiesRepository.createProperty(property, files);
+        return this.propertiesRepository.createProperty(property, files);
     }
 
     async editProperty(id: IProperties, property: IProperties): Promise<IProperties> {
-        return await this.propertiesRepository.editProperty(id, property);
+        return this.propertiesRepository.editProperty(id, property);
     }
 
     async deleteProperty(id: IProperties): Promise<void> {
-        return await this.propertiesRepository.deleteProperty(id);
+        return this.propertiesRepository.deleteProperty(id);
     }
 
     async getAllProperties(propPage: PropertyPageDto): Promise<IProperties[]> {
